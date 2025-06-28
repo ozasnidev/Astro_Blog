bf69af3b11fd1745e8e7c2cb99e6cb7d31aa86e7
Mits <ozasnidev@gmail.com>
2025-06-28 21:36:33 +0200

    Added Greeting component with Astro Island pattern

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://ozasnidev-astro-blog.netlify.app/"
+  site: "https://ozasnidev-astro-blog.netlify.app/",
+  integrations: [preact()]
 });
